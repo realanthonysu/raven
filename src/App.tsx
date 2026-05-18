804fe5fd3830d80bd9d17c60145bc515daec76c0
Anthony Su <anthonysu19@gmail.com>
2026-05-18 13:12:44 +0800

    feat: scaffold Tauri v2 + React + TypeScript project

---
+++ src/App.tsx
@@ -0,0 +1,9 @@
+function App() {
+  return (
+    <div className="h-screen flex items-center justify-center">
+      <h1 className="text-2xl font-bold">Raven</h1>
+    </div>
+  );
+}
+
+export default App;
